1ead7745ce9d4e0b90575ba6a8dfd56417913c30
HamzaAmir1470 <hamzaamir9733@gmail.com>
2026-07-26 07:56:48 +0500

    completed websocket server

--- socket/server.js
+++ socket/server.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const { WebSocketServer } = require("ws");
 const geolib = require("geolib");
+require("dotenv").config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Store driver locations
 let drivers = {};
